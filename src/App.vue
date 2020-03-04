a3697cce26eb2812e4f1674de49986731d46d125
Maya Tung <mktung0@gmail.com>
2020-03-04 08:23:06 -0500

    initial styling

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,8 @@
         <span id="temperature">{{temperature}}</span>
         <span id="summary">{{summary}}</span>
     </div>
-    <div class="form">
-        <label for="location">Enter location to get weather:</label>
+    <label for="location">Enter location to get weather:</label>
+    <div class="super-bar">
         <input v-model="loc" type="text" name="location" id="location" placeholder="latitude, longitude"/>
         <button @click="getWeather()">Go</button>
     </div>
@@ -27,7 +27,7 @@ export default {
       let location = this.loc.split(/\s*,\s*/);
       const res = await fetch(`http://localhost:3000/forecast?lat=${location[0]}&lon=${location[1]}`);
       const json = await res.json();
-      this.temperature = json.temp;
+      this.temperature = Math.floor(json.temp) + '°';
       this.summary = json.summary;
       console.log(json)
     }
@@ -36,4 +36,71 @@ export default {
 </script>
 
 <style>
+html, body {
+  margin: 0;
+  height: 100%;
+  width: 100%;
+  background: #4477FF;
+}
+
+#app {
+  font-family: Helvetica, Arial, sans-serif;
+  font-weight: bold;
+  font-size: 20px;
+  color: #FAFAFA;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 100%;
+  width: 50%;
+  margin: auto;
+}
+
+.weather-display {
+  display: flex;
+  flex-direction: column;
+  margin: 30px 0;
+}
+
+#temperature {
+  font-size: 180px;
+  line-height: 150px;
+}
+
+#summary {
+  font-size: 28px;
+}
+
+.super-bar {
+  display: flex;
+}
+
+.super-bar > * {
+  line-height: 34px;
+  height: 34px;
+  padding: 0 15px;
+  font-size: 20px;
+}
+
+.super-bar > *:first-child {
+  flex: 1 0 auto;
+  border-radius: 10px 0 0 10px;
+}
+
+.super-bar > *:last-child {
+  border-radius: 0 10px 10px 0;
+}
+
+input, button {
+  border: none;
+  background: #FAFAFA;
+}
+
+button {
+  border-left: #AFAFAF 1px solid;
+}
+
+button:hover {
+  background: #AFAFAF;
+}
 </style>
